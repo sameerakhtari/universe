134e1ccf335c30856e5303fa71a5ba17ad335d95
Sameer Akhtari <sameer_akhtari@hotmail.com>
2026-09-21 05:16:05 +0500

    Add Vite production configuration

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    target: 'es2022',
+    cssCodeSplit: false,
+    sourcemap: false,
+    chunkSizeWarningLimit: 900
+  }
+});
